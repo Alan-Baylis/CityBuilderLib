c932fd3811459d418141a76d2b953dd741be45e7
Ronald Flint <OurManFlint7@gmail.com>
2016-01-09 21:13:24 -0600

    Added members.

--- CityBuilderLib/Tile.cs
+++ CityBuilderLib/Tile.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using UnityEngine;
 
 namespace CityBuilderLib
 {
@@ -10,10 +10,19 @@ namespace CityBuilderLib
 
 	internal class BuildableTile : Tile
 	{
+		internal MapChunk ParentChunk { get; }
 
-		internal BuildableTile()
+		internal Vertex TopLeftVertex { get { return _vertices[0]; } }
+		internal Vertex TopRightVertex { get { return _vertices[1]; } }
+		internal Vertex BottomLeftVertex { get { return _vertices[0]; } }
+		internal Vertex BottomRightVertex { get { return _vertices[3]; } }
+		
+		internal BuildableTile(MapChunk parent, Vector3 originRelativeToParent, float tileSize)
 		{
 			_vertices = new Vertex[4];
+
+			float halfTileSize = (tileSize / 2);
+
 		}
 	}
 }
