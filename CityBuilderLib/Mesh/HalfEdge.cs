8382879356a801fc179e8d4f925843667b6315ba
Ronald Flint <OurManFlint7@gmail.com>
2016-01-09 21:19:00 -0600

    Early dev of mesh data structure.

---
+++ CityBuilderLib/Mesh/HalfEdge.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CityBuilderLib.Mesh
+{
+	internal class HalfEdge
+	{
+		internal Vertex PointsTo { get; set; }
+		internal HalfEdge NextEdge { get; set; }
+		internal HalfEdge OppositeEdge { get; set; }
+		internal Face Face { get; set; }
+	}
+}
---
+++ CityBuilderLib/Mesh/Vertex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using UnityEngine;
+
+namespace CityBuilderLib
+{
+	internal class Vertex
+	{
+		public Vector3 Position { get; }
+		public int Index { get; set; }
+
+		public Vertex(float x = 0.0f, float y = 0.0f, float z = 0.0f, int index = 0)
+		{
+			Position = new Vector3(x, y, z);
+		}
+	}
+}
