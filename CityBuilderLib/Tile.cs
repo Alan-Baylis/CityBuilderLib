3d55648528981d4416ffb30ebc9901e5305459b3
Ronald Flint <OurManFlint7@gmail.com>
2016-01-10 09:42:35 -0600

    Added some documentation comments.

--- CityBuilderLib/Tile.cs
+++ CityBuilderLib/Tile.cs
@@ -6,11 +6,17 @@ using CityBuilderLib.Mesh;
 
 namespace CityBuilderLib
 {
+	/// <summary>
+	/// A base tile.
+	/// </summary>
 	internal class Tile
 	{
 		protected Vertex[] _vertices;
 	}
 
+	/// <summary>
+	/// A flat tile that buildings and roads can be placed on.
+	/// </summary>
 	internal class BuildableTile : Tile
 	{
 		internal MapChunk ParentChunk { get; }
@@ -20,6 +26,12 @@ namespace CityBuilderLib
 		internal Vertex BottomLeftVertex { get { return _vertices[0]; } }
 		internal Vertex BottomRightVertex { get { return _vertices[3]; } }
 		
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="parent"></param>
+		/// <param name="originRelativeToParent"></param>
+		/// <param name="tileSize"></param>
 		internal BuildableTile(MapChunk parent, Vector3 originRelativeToParent, float tileSize)
 		{
 			_vertices = new Vertex[4];
--- CityBuilderLib/Utilities/ExceptionLogger.cs
+++ CityBuilderLib/Utilities/ExceptionLogger.cs
@@ -3,12 +3,19 @@ using System.IO;
 
 namespace CityBuilderLib.Utilities
 {
+	/// <summary>
+	/// A basic utility for logging exceptions.
+	/// </summary>
 	internal static class ExceptionLogger
 	{
 		private const string _filenamePrefix = "Exceptions ";
 		private const string _logExtension = ".log";
 		private const string _logSubDirectory = "Logs";
 
+		/// <summary>
+		/// Logs any System.Exception to a text file.
+		/// </summary>
+		/// <param name="exception">The exception to log.</param>
 		internal static void LogException(Exception exception)
 		{
 			using (var logWriter = GetLogFileWriter())
@@ -20,6 +27,10 @@ namespace CityBuilderLib.Utilities
 			}
 		}
 
+		/// <summary>
+		/// Generates a file name consisting of the year, month, day, and hour. If the file already exists, it will be opened; else it will be created.
+		/// </summary>
+		/// <returns>A valid StreamWriter for the log file to be written to.</returns>
 		private static StreamWriter GetLogFileWriter()
 		{
 			string fileName = (_filenamePrefix + DateTime.Now.ToString("yy-MM-dd-HH") + _logExtension);
